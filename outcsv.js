c0083ed38be893609051fec50b6066e964e159d5
RIORAO <lah.messagebox@gmail.com>
2021-06-21 20:38:45 +0900

    ファイルを引数で選択できるように。EOFを除く処理

--- outcsv.js
+++ outcsv.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const files = [
+let files = [
   'scrDataBoss',
   'scrDataCinem',
   'scrDataMaps',
@@ -29,6 +29,17 @@ const files = [
   'scrDataTriggerVillage'
 ];
 
+let selectedFiles = [];
+if (process.argv.length > 2) {
+  for (let i = 2; i < process.argv.length; i++) {
+    const fileName = process.argv[i];
+    if (files.includes(fileName)) {
+      selectedFiles.push(fileName);
+    }
+  }
+  files = selectedFiles;
+}
+
 let sentenceCount = 0;
 let translatedCount = 0;
 let outData = 'key, ja, en, translated\n';
@@ -40,6 +51,9 @@ for (let i = 0; i < files.length; i++) {
   const jsonEn = JSON.parse(fs.readFileSync(`./en/${readFileName}`, 'utf8')).scr;
 
   for (const key in jsonJa) {
+    if (key === 'EOF') {
+      continue;
+    }
     const translated = jsonJa[key] !== jsonEn[key] ? 1 : 0;
     outData += `${key}, "${jsonJa[key]}", "${jsonEn[key]}", ${translated}\n`;
     sentenceCount++;
